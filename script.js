c1a090982bc97c3a07cb7570750c781af37fd854
youweb3 <yousef.shaikhan@gmail.com>
2022-11-18 22:39:30 +0000

    300

--- script.js
+++ script.js
@@ -13,15 +13,17 @@ function makePageForEpisodes(episodeList) {
   for (let episode of episodeList) {
     
     let episodeListItem = document.createElement ("li");
+    let title = document.createElement("h1");
+    episodeListItem.setAttribute("id", episode.name);
     let h3Element = document.createElement("h3");
     let image = document.createElement("img");
     let paragraph = document.createElement("p");
-    episodeListItem.innerText =`${episode.name}`
+    title.innerText =`${episode.name}`
     h3Element.textContent = `S0${episode.season}E${episode.number >= 10 ? episode.number:"0"+ episode.number}`;
     image.src=`${episode.image.medium}`
     paragraph.innerHTML = episode.summary;
 
-    
+    episodeListItem.appendChild(title);
     episodeListItem.appendChild(h3Element);
     episodeListItem.appendChild(image);
     episodeListItem.appendChild(paragraph);
@@ -34,35 +36,18 @@ function makePageForEpisodes(episodeList) {
 
 }
 
-
-// search
-const form=document.getElementById("form");
-  const searchInput = document.getElementById("search");
-  let episodeList=getAllEpisodes();
-
-  searchInput.addEventListener("keyup",(e)=>{
-    let value =e.target.value;
-    let html = "";
-    episodeList.forEach((element)=>{
-      if (element.name.toLowerCase().includes(value) || element.summary.toLowerCase().includes(value)) {
-         html += `<div>
-        <h3>${element.name}</h3>
-        <img src=${element.image.medium}>
-        <p>${element.summary}</p>
-        </div>`;
-
-      }
-    })
-
-   rootElem.innerHTML = html;
-  })
-
-
-
-//dropdown list
+function handleChosenEpisode(event) {
+  let opts = event.target.selectedOptions;
+  if (opts.length !== 1) {
+      return;
+  }
+  let id = opts[0].value;
+  document.location.assign(`#${id}`);
+}
+//dropdown list///
   const searchEpisod= getAllEpisodes();
   const select = document.getElementById("movies");
-
+select.onchange = handleChosenEpisode;
    searchEpisod.forEach((episod) => {
      const option = document.createElement("option");
 
@@ -75,22 +60,7 @@ const form=document.getElementById("form");
        option.textContent = `S${episod.number}E${episod.season}-${episod.name}`;
      }
    });
-//drop down search
-     select.addEventListener("change", () => {
-       let dropValue = select.value;
-       let html = "";
-      searchEpisod.forEach((episod)=>{
-        if (episod.name.includes(dropValue)) {
-          html += `<div>
-        <h3>${episod.name}</h3>
-        <img src=${episod.image.medium}>
-        <p>${episod.summary}</p>
-        </div>`;
-        }
-      })
 
-     rootElem.innerHTML = html;
-   });
 
 
 window.onload = setup;
